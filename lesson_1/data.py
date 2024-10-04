a7065601f9b52e97396f0a7837b898329e9cbc62
Ksenia Kuzmicheva <39733280+KuzmichevaKsenia@users.noreply.github.com>
2024-10-04 17:59:09 +0000

    fix id

--- lesson_1/data.py
+++ lesson_1/data.py
@@ -1,8 +1,8 @@
 employees = {
-    0: {"id": 0, "name": "Kakashi Hatake", "position": "Teacher"},
     1: {"id": 1, "name": "Piter Parker", "position": "Photographer"},
     2: {"id": 2, "name": "Sherlock Holmes", "position": "Detective"},
     3: {"id": 3, "name": "Rick Sanchez", "position": "Scientist"},
     4: {"id": 4, "name": "Jimmy McGill", "position": "Lawyer"},
-    5: {"id": 5, "name": "Stephen Strange", "position": "Neurosurgery"}
+    5: {"id": 5, "name": "Stephen Strange", "position": "Neurosurgery"},
+    6: {"id": 6, "name": "Kakashi Hatake", "position": "Teacher"},
 }
